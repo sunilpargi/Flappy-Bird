77ce16940fd9e2797afbd12bab0daba34d12170f
Sunil pargi <sunilpargi121@gmail.com>
2020-12-02 19:31:44 +0530

    added bird movement, pipe spawning, BG spawning, Audio Source

---
+++ Flappy Bird/Assets/_Scripts/Camera/CameraScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+	public static float offsetX;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (BIrd.instance != null)
+		{
+			if (BIrd.instance.isAlive)
+			{
+				MoveTheCamera();
+			}
+		}
+	}
+
+	void MoveTheCamera()
+	{
+		Vector3 temp = transform.position;
+		temp.x = BIrd.instance.GetPositionX() + offsetX;
+		transform.position = temp;
+	}
+}
